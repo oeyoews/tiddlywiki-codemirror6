fc361878631226e7d3b7a36dfb81f0c869c6db8e
oeyoews <jyao4783@gmail.com>
2024-01-02 03:12:00 +0800

    feat: support tw5 snippets

--- src/tiddlywiki-codemirror-6/modules/widgetCompletions.ts
+++ src/tiddlywiki-codemirror-6/modules/widgetCompletions.ts
@@ -1,12 +1,12 @@
 import { CompletionContext } from '@codemirror/autocomplete';
 import getAllWidget from '../utils/getAllWidget';
+import getAllSnippets from '../utils/getAllSnippet';
 // import { syntaxTree } from '@codemirror/language';
 
 // https://codemirror.net/examples/autocompletion/
 export default function widgetCompletions(context: CompletionContext) {
-  // 最小补全 length
-  // const word = context.matchBefore(/\w*/); // 这种遇到短横线会消失，但是不影响多次触发同一行内
-  const word = context.matchBefore(/<\$/);
+  const word = context.matchBefore(/\w*/); // 这种遇到短横线会消失，但是不影响多次触发同一行内
+  // const word = context.matchBefore(/<\$/);
   // let nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1);
   // console.log(nodeBefore.from);
   if (!word || (word.from == word.to && !context.explicit)) return null;
@@ -15,15 +15,10 @@ export default function widgetCompletions(context: CompletionContext) {
   return {
     from: word.from,
     options: [
-      {
-        label: 'match',
-        type: 'keyword',
-        apply: '替换后的文本：match',
-        detail: '详情 match(测试)'
-      },
       // { label: 'hello', type: 'variable', info: '(World)' },
       // { label: 'magic', type: 'text', apply: '⠁⭒*.✩.*⭒⠁', detail: 'macro' },
-      ...getAllWidget()
+      ...getAllWidget(),
+      ...getAllSnippets()
     ],
     validFor: /^[\w$]*$/
   };
--- src/tiddlywiki-codemirror-6/utils/getAllSnippet.ts
+++ src/tiddlywiki-codemirror-6/utils/getAllSnippet.ts
@@ -1,1 +1,26 @@
-// https://discuss.codemirror.net/t/adding-custom-snippets/3535/3
+import { snippetCompletion } from '@codemirror/autocomplete';
+
+export default function getAllSnippets() {
+  const snippetTiddlers = $tw.wiki.filterTiddlers(
+    '[all[shadows+system+tiddlers]tag[$:/tags/TextEditor/Snippet]!has[draft.of]]'
+  );
+
+  const allInfo = snippetTiddlers.map((title) => {
+    const { caption = '', text = '' } = $tw.wiki.getTiddler(title)?.fields!;
+    return {
+      title: title.split('/').pop()!,
+      text,
+      caption
+    };
+  });
+
+  return allInfo.map((info) =>
+    snippetCompletion(`${info.text}`, {
+      label: info.title,
+      displayLabel: info.caption as string, // 显示字符
+      type: 'keyword',
+      apply: info.text,
+      info: info.text
+    })
+  );
+}
