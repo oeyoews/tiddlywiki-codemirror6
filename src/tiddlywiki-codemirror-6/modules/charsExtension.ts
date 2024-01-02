96feeabac00083275612651dbfe7c2872641fc25
oeyoews <jyao4783@gmail.com>
2024-01-02 22:01:23 +0800

    update

--- src/tiddlywiki-codemirror-6/modules/charsExtension.ts
+++ src/tiddlywiki-codemirror-6/modules/charsExtension.ts
@@ -1,20 +1,6 @@
 import { showPanel, EditorView, Panel } from '@codemirror/view';
 import type { Text } from '@codemirror/state';
 
-// function countWords(doc: Text) {
-//   let count = 0,
-//     iter = doc.iter();
-//   while (!iter.next().done) {
-//     let inWord = false;
-//     for (let i = 0; i < iter.value.length; i++) {
-//       let word = /[\w\u4e00-\u9fa5]/.test(iter.value[i]);
-//       // if (word && !inWord)
-//       count++;
-//       inWord = word;
-//     }
-//   }
-//   return `word count: ${count} (${doc.length} chars)`;
-// }
 function countWords(doc: Text) {
   let count = 0,
     iter = doc.iter();
@@ -23,7 +9,6 @@ function countWords(doc: Text) {
     let inWord = false;
 
     for (let i = 0; i < iter.value.length; i++) {
-      // 修改正则表达式以包括字母、数字和 CJK 字符
       let word = /[\w\u4e00-\u9fa5]/.test(iter.value[i]);
       let isChineseChar = /[\u4e00-\u9fa5]/.test(iter.value[i]);
 
