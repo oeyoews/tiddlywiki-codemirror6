c466eb4ee0b1980d75fa18092ee8e86581b2df4f
oeyoews <jyao4783@gmail.com>
2024-01-03 02:01:37 +0800

    add dist lang-tiddlywiki

---
+++ packages/lang-tiddlywiki/dist/index.d.ts
@@ -0,0 +1,4 @@
+import { LRLanguage, LanguageSupport } from '@codemirror/language';
+declare const tiddlywikiLanguage: LRLanguage;
+declare function tiddlywiki(): LanguageSupport;
+export { tiddlywikiLanguage, tiddlywiki };
