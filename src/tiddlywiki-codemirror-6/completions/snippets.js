a81e7d129292e4410ac86cf2cb05c4518731deb5
oeyoews <jyao4783@gmail.com>
2024-01-04 18:21:58 +0800

    update

---
+++ src/tiddlywiki-codemirror-6/completions/snippets.js
@@ -0,0 +1,18 @@
+// wikitext
+
+// markdown
+
+// js
+
+// TODO
+
+const markdownsnippets = [
+  {
+    title: 'codeblcok',
+    snippet: '```${1:lang}\n ${2} ```'
+  }
+];
+
+export default builtinSnippets = {
+  markdownsnippets
+};
