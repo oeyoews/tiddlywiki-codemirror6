52c432b7baaaa5bdaa8bb4d2499e5e0712b57cff
oeyoews <jyao4783@gmail.com>
2024-01-05 00:36:49 +0800

    feat: add bump version js

--- scripts/bump.js
+++ scripts/bump.js
@@ -1,6 +1,29 @@
-// bump version
 const fs = require('fs');
 const path = require('path');
 
-const infoFile = path.join(__dirname, 'src/tiddlywiki-codemirror-6');
-const info = fs.readFileSync();
+const infoFilePath = path.join(
+  __dirname,
+  '..',
+  'src/tiddlywiki-codemirror-6/plugin.info'
+);
+
+// 读取文件内容
+const infoFileContent = fs.readFileSync(infoFilePath, 'utf8');
+
+// 将 JSON 字符串转换成对象
+const info = JSON.parse(infoFileContent);
+
+// 递增版本号的末尾数字
+const currentVersion = info.version.split('.').map(Number); // 将版本号字符串转换为数字数组
+currentVersion[2] += 1; // 递增末尾数字
+
+// 更新对象中的版本号
+info.version = currentVersion.join('.');
+
+// 将更新后的对象转换回 JSON 字符串
+const updatedInfoFileContent = JSON.stringify(info, null, 2);
+
+// 将更新后的内容写回文件
+fs.writeFileSync(infoFilePath, updatedInfoFileContent, 'utf8');
+
+console.log('版本号已成功递增！', info.version);
