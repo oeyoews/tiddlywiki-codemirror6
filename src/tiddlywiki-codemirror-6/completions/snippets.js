12b350c28ee4b283247dd5b131ac5c8dab5d4747
oeyoews <jyao4783@gmail.com>
2024-01-06 00:15:30 +0800

    fix: update tabsize filename and add time snippets

--- src/tiddlywiki-codemirror-6/completions/snippets.js
+++ src/tiddlywiki-codemirror-6/completions/snippets.js
@@ -10,6 +10,14 @@ export const usersnippets = [
   {
     title: 'codeblcok',
     text: '```${1:lang}\n${2:code}\n```'
+  },
+  {
+    title: 'date',
+    text: `${new Date().toISOString().split('T')[0]}`
+  },
+  {
+    title: 'time',
+    text: `${new Date().toISOString().split('.')[0].replace('T', ' ')}`
   }
 ];
 
