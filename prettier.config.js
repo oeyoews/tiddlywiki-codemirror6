673183588d10a0db89cd5c8a207be8799a51f643
oeyoews <jyao4783@gmail.com>
2024-01-08 01:46:53 +0800

    update

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  arrowParens: 'avoid',
-  trailingComma: 'es6',
-  trailingComma: 'all',
-  singleQuote: true,
-  endOfLine: 'lf'
-};
---
+++ src/tiddlywiki-codemirror-6/test.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+
+class Test {
+  constructor() {
+    this.name = 99;
+  }
+}
