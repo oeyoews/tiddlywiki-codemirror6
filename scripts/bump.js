37ef8edf36afa2baaf0c09bf269425083245eaea
oeyoews <jyao4783@gmail.com>
2024-01-08 03:59:24 +0800

    bump version

--- scripts/bump.js
+++ scripts/bump.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const jsonInfo = require('../package.json');
+const langtwInfo = require('../packages/lang-tiddlywiki/package.json');
 
 const infoFilePath = path.join(
   __dirname,
@@ -21,7 +22,7 @@ currentVersion[2] += 1; // 递增末尾数字
 // 更新对象中的版本号
 info.version = currentVersion.join('.');
 jsonInfo.version = info.version;
-console.log(jsonInfo.version);
+langtwInfo.version = info.version;
 
 // 将更新后的对象转换回 JSON 字符串
 const updatedInfoFileContent = JSON.stringify(info, null, 2);
@@ -29,5 +30,10 @@ const updatedInfoFileContent = JSON.stringify(info, null, 2);
 // 将更新后的内容写回文件
 fs.writeFileSync(infoFilePath, updatedInfoFileContent, 'utf8');
 fs.writeFileSync('./package.json', JSON.stringify(jsonInfo, null, 2), 'utf8');
+fs.writeFileSync(
+  './packages/lang-tiddlywiki/package.json',
+  JSON.stringify(langtwInfo, null, 2),
+  'utf8'
+);
 
 console.log('版本号已更新！', info.version);
