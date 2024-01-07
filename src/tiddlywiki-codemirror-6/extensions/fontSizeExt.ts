29ace9f58dad2db5ff35221fc3c125fc99933210
oeyoews <jyao4783@gmail.com>
2024-01-07 20:51:56 +0800

    update

--- src/tiddlywiki-codemirror-6/extensions/fontSizeExt.ts
+++ src/tiddlywiki-codemirror-6/extensions/fontSizeExt.ts
@@ -1,6 +1,7 @@
 import { EditorView } from '@codemirror/view';
 import cmeConfig from '../cmeConfig';
 
+/** @see-also https://codemirror.net/examples/styling */
 export default function () {
   return EditorView.theme({
     '&.cm-editor': {
