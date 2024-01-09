0fc75f736195a299f13d270790127e88afc2882f
oeyoews <jyao4783@gmail.com>
2024-01-09 18:21:55 +0800

    update

--- src/tiddlywiki-codemirror-6/completions/sources.ts
+++ src/tiddlywiki-codemirror-6/completions/sources.ts
@@ -6,8 +6,9 @@ import {
 import delimiter from '../utils/triggerType';
 import conf from '../cmeConfig';
 import type { ISource } from '../types';
-import { CompletionSection } from '@codemirror/autocomplete';
+// import { CompletionSection } from '@codemirror/autocomplete';
 
+// TODO: add /settings to jump setup tiddler
 // 如果不对 label 进行特殊处理，就要处理光标位置，自定义 app function, 灵活性较差 https://github.com/BurningTreeC/tiddlywiki-codemirror-6/blob/6ed53e8624b12cf2c09187f4f5fdcdd5960889c3/plugins/tiddlywiki-codemirror-6/engine.js#L327-L346C3
 function getImageSnippets() {
   const allImageTiddlers = $tw.wiki.filterTiddlers(
