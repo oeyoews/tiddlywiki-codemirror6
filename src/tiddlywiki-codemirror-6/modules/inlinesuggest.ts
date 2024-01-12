173671b44e97c0f3bae7c2312b2d0beb76a09291
oeyoews <jyao4783@gmail.com>
2024-01-13 00:12:20 +0800

    update

--- src/tiddlywiki-codemirror-6/modules/inlinesuggest.ts
+++ src/tiddlywiki-codemirror-6/modules/inlinesuggest.ts
@@ -15,13 +15,37 @@ export default function inlineSuggestionExt(self: {
   // status wrong
   const fetchSuggestion = () => {
     const state = self.cm.state;
+
+    const cursorPos = state.selection.main.head;
+    const doc = state.doc;
+
+    let wordStart = cursorPos;
+
+    while (
+      wordStart > 0 &&
+      /[^\s]/.test(doc.sliceString(wordStart - 1, wordStart)) // 使用 [^\s] 表示非空白字符
+    ) {
+      wordStart--;
+    }
+
+    // 获取光标位置前最后一个单词
+    let lastWord = doc.sliceString(wordStart, cursorPos);
+
+    // 中文是一个汉字等于两个英文字符
+    if (lastWord.length < cmeConfig.minLength() || wordStart === cursorPos) {
+      return;
+    }
+
+    console.log(lastWord);
+
     const status = completionStatus(state) === 'active';
     if (!status) {
       return;
     }
 
     // TODO: 需要细化逻辑
-    return selectedCompletion(state)?.label;
+    // return selectedCompletion(state)?.label.slice(0, lastWord.length);
+    return selectedCompletion(state)?.displayLabel;
   };
 
   if (cmeConfig.inlineSuggestion()) {
