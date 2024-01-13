e5e99f0952bc3150b7f00d6691c237562c919830
oeyoews <jyao4783@gmail.com>
2024-01-13 23:29:41 +0800

    fix: use transclude $index to instead of try catch

--- src/tiddlywiki-codemirror-6/utils/renderTiddler.ts
+++ src/tiddlywiki-codemirror-6/utils/renderTiddler.ts
@@ -4,8 +4,6 @@ export function renderTid(title: string | undefined): CompletionInfo | null {
   if (!title) return null;
   if ($tw.wiki.getTiddler(title)?.fields.render === 'false') return null;
   if (!conf.tiddlerPreview()) return null;
-  // NOTE: 无法检测到系统 tiddler
-  // if (!$tw.wiki.tiddlerExists(title)) return null;
 
   if (!$tw.wiki.getTiddlerText(title)) {
     const titleNode = document.createElement('h2');
@@ -15,13 +13,17 @@ export function renderTid(title: string | undefined): CompletionInfo | null {
   // NOTE: 如果需要解析为 inline 的话，会导致 !! 这种 wikitext 的语法 parse 错误
   let previewHTML = '暂不支持预览';
   const preview = document.createElement('div');
-  try {
-    previewHTML = $tw.wiki.renderTiddler('text/html', title, {
-      // parseAsInline: false
-    });
-    if (!previewHTML) return null;
-    preview.innerHTML = previewHTML;
-    preview.className = 'cm-image-preview';
-  } catch (e) {}
+  // try {
+  previewHTML = $tw.wiki.renderText(
+    'text/html',
+    title,
+    `<$transclude $tiddler=${title} $index=${title} />`
+  );
+  if (!previewHTML || previewHTML === '<p></p>') return null;
+  preview.innerHTML = previewHTML;
+  preview.className = 'cm-image-preview';
+  // } catch (e) {
+  //   console.log(e);
+  // }
   return preview;
 }
