b37e2cd45c5357547807bb88cb37f7a392726004
oeyoews <jyao4783@gmail.com>
2024-01-13 16:17:54 +0800

    refactor cm6 config

--- src/tiddlywiki-codemirror-6/cm6.ts
+++ src/tiddlywiki-codemirror-6/cm6.ts
@@ -20,55 +20,158 @@ function getConfig(title: string) {
 
 // TODO: 这些配置如果写成常量，tiddlywiki 将会缓存？??，不会每次重新计算 (不知道为什么), 实例仅仅创建了一次？??，但是为什么做成 function, 重新在实例里面计算又可以了？??
 // NOTE: 对应配置的 caption 不要写成 TxxxBxxx 格式。
-export const titles = [
-  'inlineSuggestion',
-  'tiddlerPreview',
-  'snippetPreview',
-  'linkPreview',
-  'fontsize',
-  'enableMarkdownJsHighlight',
-  'enableWordCount',
-  'highlightWhitespace',
-  'highlightTrailingWhitespace',
-  'enableSystemTiddlersCompletion',
-  'closeOnBlur',
-  'foldGutter',
-  'enableOneDarkTheme',
-  'clickable-icon',
-  'clickable',
-  'customPlaceholder',
-  'placeholder',
-  'cursorBlinkRate',
-  'minLength',
-  'delimiter',
-  'minimap',
-  'closeBrackets',
-  'selectOnOpen',
-  'autocompleteIcons',
-  'maxRenderedOptions',
-  'spellcheck',
-  'autocorrect',
-  'indentWithTab',
-  'bracketMatching',
-  'vimmode',
-  'completeAnyWord',
-  'bracketMatching',
-  'lineNumbers',
-  'highlightActiveLine',
-  'tabSize',
-  'activateOnTyping'
-] as const;
+export const tiddlers = {
+  inlineSuggestion: {
+    caption: 'inline suggestion(experimental)',
+    description: 'inline suggestion(experimental)'
+  },
+  tiddlerPreview: {
+    caption: 'tiddler preview',
+    description: 'tiddler preview'
+  },
+  snippetPreview: {
+    caption: 'snippet preview',
+    description: 'snippet preview'
+  },
+  linkPreview: {
+    caption: 'link preview',
+    description: 'link preview'
+  },
+  fontsize: {
+    caption: 'font size',
+    description: 'font size'
+  },
+  enableMarkdownJsHighlight: {
+    caption: 'enable Markdown JS highlight',
+    description: 'enabling Markdown JS highlight'
+  },
+  enableWordCount: {
+    caption: 'enable word count',
+    description: 'enabling word count'
+  },
+  highlightWhitespace: {
+    caption: 'highlight whitespace',
+    description: 'highlighting whitespace'
+  },
+  highlightTrailingWhitespace: {
+    caption: 'highlight trailing whitespace',
+    description: 'highlighting trailing whitespace'
+  },
+  enableSystemTiddlersCompletion: {
+    caption: 'enable system tiddlers completion',
+    description: 'enabling system tiddlers completion'
+  },
+  closeOnBlur: {
+    caption: 'close on blur',
+    description: 'closing on blur'
+  },
+  foldGutter: {
+    caption: 'fold gutter',
+    description: 'fold gutter'
+  },
+  enableOneDarkTheme: {
+    caption: 'enable One Dark theme',
+    description: 'enabling One Dark theme'
+  },
+  'clickable-icon': {
+    caption: 'clickable icon',
+    description: 'clickable icon'
+  },
+  clickable: {
+    caption: 'clickable',
+    description: 'clickable'
+  },
+  customPlaceholder: {
+    caption: 'custom placeholder',
+    description: 'custom placeholder'
+  },
+  placeholder: {
+    caption: 'placeholder',
+    description: 'placeholder'
+  },
+  cursorBlinkRate: {
+    caption: 'cursor blink rate',
+    description: 'cursor blink rate'
+  },
+  minLength: {
+    caption: 'min length',
+    description: 'min length'
+  },
+  delimiter: {
+    caption: 'delimiter',
+    description: 'delimiter'
+  },
+  minimap: {
+    caption: 'minimap',
+    description: 'minimap'
+  },
+  closeBrackets: {
+    caption: 'close brackets',
+    description: 'closing brackets'
+  },
+  selectOnOpen: {
+    caption: 'select on open',
+    description: 'selecting on open'
+  },
+  autocompleteIcons: {
+    caption: 'autocomplete icons',
+    description: 'autocomplete icons'
+  },
+  maxRenderedOptions: {
+    caption: 'max rendered options',
+    description: 'max rendered options'
+  },
+  spellcheck: {
+    caption: 'spellcheck',
+    description: 'spellcheck'
+  },
+  autocorrect: {
+    caption: 'autocorrect',
+    description: 'autocorrect'
+  },
+  indentWithTab: {
+    caption: 'indent with tab',
+    description: 'indenting with tab'
+  },
+  bracketMatching: {
+    caption: 'bracket matching',
+    description: 'bracket matching'
+  },
+  vimmode: {
+    caption: 'Vim mode',
+    description: 'Vim mode'
+  },
+  completeAnyWord: {
+    caption: 'complete any word',
+    description: 'completing any word'
+  },
+  lineNumbers: {
+    caption: 'line numbers',
+    description: 'line numbers'
+  },
+  highlightActiveLine: {
+    caption: 'highlight active line',
+    description: 'highlighting active line'
+  },
+  tabSize: {
+    caption: 'tab size',
+    description: 'tab size'
+  },
+  activateOnTyping: {
+    caption: 'activate on typing',
+    description: 'activating on typing'
+  }
+};
 
-type IOptions = (typeof titles)[number];
+type IOptions = keyof typeof tiddlers;
+
 type IConfig = Record<IOptions, () => any>;
 
-// export const config: Map<IOptions, string | boolean> = new Map();
 const cm6 = {} as IConfig;
+const options = Object.keys(tiddlers) as IOptions[];
 
-titles.forEach((title) => {
-  // NOTE: 需要每次重新计算新值，
-  // config.set(title, getConfig(title)!);
-  cm6[title] = () => getConfig(title)!;
+options.forEach((key: IOptions) => {
+  cm6[key] = () => getConfig(key) as any;
 });
 
 export default cm6;
