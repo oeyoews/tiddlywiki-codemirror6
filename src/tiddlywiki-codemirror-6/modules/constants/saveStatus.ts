28852b366bbd76f64ef75ae9f3f3ff56b3b8fc51
oeyoews <jyao4783@gmail.com>
2024-02-07 12:00:48 +0800

    fix: use state instead config prefix

--- src/tiddlywiki-codemirror-6/modules/constants/saveStatus.ts
+++ src/tiddlywiki-codemirror-6/modules/constants/saveStatus.ts
@@ -1,4 +1,4 @@
-export const statusTiddler = '$:/config/oeyoews/codemirror6/status/save';
+export const statusTiddler = '$:/state/oeyoews/codemirror6/status/save';
 
 export function updateSaveStatus(status: boolean) {
   $tw.wiki.setText(statusTiddler, 'text', '', status ? 'yes' : 'no');
