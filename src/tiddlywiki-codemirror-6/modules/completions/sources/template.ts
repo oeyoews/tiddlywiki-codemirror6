57866de0bf21a6680edd8dfcefb56d2a2dbcdd18
oeyoews <jyao4783@gmail.com>
2024-09-22 22:34:58 +0800

    feat: support template preview

--- src/tiddlywiki-codemirror-6/modules/completions/sources/template.ts
+++ src/tiddlywiki-codemirror-6/modules/completions/sources/template.ts
@@ -2,17 +2,8 @@
 // add new snippet
 // add new template
 import { Completion } from '@codemirror/autocomplete';
-import cm6, { configBaseTitle } from '@/cm6/config';
 import { IWidget } from '@/cm6/types/IWidget';
 import { EditorView } from '@codemirror/view';
-
-const items = $tw.wiki
-  .filterTiddlers(
-    '[all[shadows+tiddlers]tag[$:/tags/TextEditor/Templates]] [prefix[$:/templates/]] -[is[draft]]'
-  )
-  .map((item) => ({
-    title: item
-  }));
 
 // export type IEventTypes = (typeof filetypes)[number]['title'];
 
@@ -20,14 +11,42 @@ const section = 'template';
 const type = 'cm-template';
 const delimiter = '@t';
 
+const renderInfo = (tiddler: string) => {
+  const {
+    bag,
+    title,
+    revision,
+    creator,
+    created,
+    modified,
+    modifier,
+    ...fields
+  } = $tw.wiki.getTiddler(tiddler)!.fields;
+  const tableNode = document.createElement('table');
+  Object.entries(fields).forEach(([key, value]) => {
+    return (tableNode.innerHTML += `<tr><td>${key}</td><td>${value}</td></tr>`);
+  });
+  // console.log(tableNode);
+  return tableNode;
+};
+
 function snippets(widget: IWidget) {
+  const items = $tw.wiki
+    .filterTiddlers(
+      '[all[shadows+tiddlers]tag[$:/tags/TextEditor/Templates]] [prefix[$:/templates/]] -[is[draft]]'
+    )
+    .map((item) => ({
+      title: item
+    }));
+
   return items.map(
     (item) =>
       ({
         section,
         label: delimiter + item.title,
-        displayLabel: item.title, // displayLabel 会影响match underline
+        displayLabel: item.title.split('/').pop()!, // displayLabel 会影响match underline
         type,
+        info: () => renderInfo(item.title),
         apply: (view: EditorView, completion: Completion, from, to) => {
           // $tw.wiki.setText(widget.editTitle, 'draft.title', '', item.title);
           const {
@@ -39,8 +58,21 @@ function snippets(widget: IWidget) {
             modifier,
             ...fields
           } = $tw.wiki.getTiddler(item.title)!.fields;
-          // todo: 清空旧的field
-          // $tw.wiki.setText(widget.editTitle, 'cap', '', undefined);
+          const {
+            title: oldTitle,
+            'draft.title': oldDTitle,
+            'draft.of': oldTitleOf,
+            revision: oldRevision,
+            creator: oldCreator,
+            created: oldCreated,
+            modified: oldModified,
+            modifier: oldModifier,
+            ...oldfields
+          } = $tw.wiki.getTiddler(widget.editTitle)!.fields;
+
+          Object.entries(oldfields).forEach(([key]) => {
+            $tw.wiki.setText(widget.editTitle, key, '', undefined);
+          });
           Object.entries(fields).forEach(([key, value]) => {
             $tw.wiki.setText(widget.editTitle, key, '', value as any);
           });
