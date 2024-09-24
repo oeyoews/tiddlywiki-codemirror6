4aee662c53b72d6dae6e983a3ef19ee32c6f84f0
oeyoews <jyao4783@gmail.com>
2024-09-24 20:51:14 +0800

    feat: help trigger support completion twice

--- src/tiddlywiki-codemirror-6/modules/completions/sources/help.ts
+++ src/tiddlywiki-codemirror-6/modules/completions/sources/help.ts
@@ -1,20 +1,37 @@
 import {
   Completion,
-  snippetCompletion as snip
+  snippetCompletion as snip,
+  startCompletion
 } from '@codemirror/autocomplete';
 import { delimitersInfo } from '.';
+import { IWidget } from '@/cm6/types/IWidget';
 
 const section = 'help';
 const type = 'cm-help';
 const delimiter = '/?';
 const description = 'show help triggers';
 
-function snippets(): Completion[] {
+function snippets(widget: IWidget): Completion[] {
   const items = delimitersInfo.map((item) => ({
     section: item.section,
     delimiter: item.delimiters,
     type: item.type,
     info: item.description
+  }));
+
+  return items.map((item) => ({
+    section,
+    label: delimiter + item.section,
+    detail: item.delimiter,
+    info: item.info,
+    displayLabel: item.section,
+    apply: (view, completion: Completion, from, to) => {
+      view.dispatch({
+        changes: { from, to, insert: item.delimiter }
+      });
+      startCompletion(view);
+    },
+    type: item.type
   }));
 
   return items.map((item) =>
