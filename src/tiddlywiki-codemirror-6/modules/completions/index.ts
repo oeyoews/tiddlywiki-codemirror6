0a6aeac5dabae2ddfeb264a2beddb2f378d9647a
oeyoews <jyao4783@gmail.com>
2024-10-07 14:28:29 +0800

    fix: better fields word check

--- src/tiddlywiki-codemirror-6/modules/completions/index.ts
+++ src/tiddlywiki-codemirror-6/modules/completions/index.ts
@@ -53,7 +53,7 @@ export default (widget: IWidget, self: any) => {
     function checkFieldStart(word: string) {
       // ：
       const regex =
-        /^@[a-zA-Z0-9_\u4e00-\u9fa5]+[:][a-zA-Z0-9_\u4e00-\u9fa5]+$/;
+        /^@[a-zA-Z0-9_-\u4e00-\u9fa5]+[:][a-zA-Z0-9_-\u4e00-\u9fa5]+$/;
 
       return regex.test(word);
     }
