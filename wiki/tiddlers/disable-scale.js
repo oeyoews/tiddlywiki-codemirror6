27a40fc4558fef050838ffaf2ff4dd69c43d14e5
oeyoews <jyao4783@gmail.com>
2024-10-15 14:37:21 +0800

    test: add disable-scale.js

---
+++ wiki/tiddlers/disable-scale.js
@@ -0,0 +1,34 @@
+/*\
+title:disable scale
+type: application/javascript
+module-type: startup
+
+\*/
+exports.name = 'scale';
+exports.platforms = ['browser'];
+exports.after = ['startup'];
+exports.synchronous = true;
+exports.startup = () => {
+ 
+
+
+console.log('startup disable scale')
+window.onload = () => {
+		console.log('onload')
+  document.addEventListener('touchstart', (event) => {
+		console.log('touchstart')
+    if (event.touches.length > 1) {
+       event.preventDefault();
+    }
+  });
+  
+  let lastTouchEnd = 0;
+  document.addEventListener('touchend', (event) => {
+    const now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  }, false);
+}
+	};
